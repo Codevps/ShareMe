a2b610c8a280a88e870310364f7d20401c0633c2
Pratham Sawant <codevps07@gmail.com>
2022-09-03 18:57:51 +0530

    working frontend

--- client/src/pages/RightSide/RightSide.jsx
+++ client/src/pages/RightSide/RightSide.jsx
@@ -1,5 +1,5 @@
 import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
-import { Button, IconButton } from "@mui/material";
+import { Button, IconButton, Paper } from "@mui/material";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ShareModal from "../PostSide/ShareModal.jsx";
@@ -7,6 +7,7 @@ import Trends from "./Trends.jsx";
 
 const RightSide = () => {
   const [open, setOpen] = useState(false);
+  const [show, setShow] = useState(false);
   const navigate = useNavigate();
   return (
     <div className="RightSide">
@@ -24,9 +25,56 @@ const RightSide = () => {
         >
           <i class="fa-solid fa-house-user" style={{ color: "#ff5349" }}></i>
         </IconButton>
-        <IconButton style={{ color: "black", fontSize: "2rem" }}>
-          <i class="fa-solid fa-gear"></i>
-        </IconButton>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          <IconButton
+            style={{ color: "black", fontSize: "2rem" }}
+            onClick={() => setShow((prevShow) => !prevShow)}
+          >
+            <i class="fa-solid fa-gear"></i>
+          </IconButton>
+          {/* <div className="dropdown"> */}
+          {show && (
+            <Paper
+              style={{
+                marginTop: "3rem",
+                position: "absolute",
+                backgroundColor: "#fed8b1",
+                opacity: "0.9",
+                display: "flex",
+                flexDirection: "column",
+                padding: "1.5rem",
+                zIndex: "1000",
+              }}
+            >
+              <p
+                variant="h6"
+                style={{ textTransform: "uppercase", color: "black" }}
+              >
+                <b>Pratham</b>
+              </p>
+              <Button
+                // className={classes.btn}
+                variant="contained"
+                style={{
+                  color: "black",
+                  border: "1px solid black",
+                  backgroundColor: "transparent",
+                  margin: "0.5rem",
+                }}
+                onClick={() => {}}
+              >
+                Logout
+              </Button>
+            </Paper>
+          )}
+        </div>
         <IconButton style={{ color: "black", fontSize: "2rem" }}>
           <QuestionAnswerIcon fontSize="large" />
         </IconButton>
